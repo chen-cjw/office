8a7efd183dce285def09d854787a1d24e59469e8
柚子 <106157421@qq.com>
2020-04-22 12:01:40 +0800

    获取用户信息

--- app/Http/Controllers/Api/V1/WeChatController.php
+++ app/Http/Controllers/Api/V1/WeChatController.php
@@ -18,11 +18,11 @@ class WeChatController extends Controller
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $app = app('wechat.official_account');
-        $app->server->push(function($message){
-//            return "欢迎关注 overtrue！";
-            return session('wechat.oauth_user.default'); // 拿到授权用户资料
-        });
-//        $user = session('wechat.oauth_user.default'); // 拿到授权用户资料
+//        $app->server->push(function($message){
+////            return "欢迎关注 overtrue！";
+//            $user = session('wechat.oauth_user.default'); // 拿到授权用户资料
+//        });
+        $user = session('wechat.oauth_user.default'); // 拿到授权用户资料
 
         dd($user);
 
