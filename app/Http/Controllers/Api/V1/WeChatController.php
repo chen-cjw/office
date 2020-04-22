eef533b5a18db9cc0925da3d02e24da0d3df50b4
柚子 <106157421@qq.com>
2020-04-22 11:58:50 +0800

    获取用户信息

--- app/Http/Controllers/Api/V1/WeChatController.php
+++ app/Http/Controllers/Api/V1/WeChatController.php
@@ -19,7 +19,7 @@ class WeChatController extends Controller
 
         $app = app('wechat.official_account');
         $app->server->push(function($message){
-            return "欢迎关注 overtrue！";
+//            return "欢迎关注 overtrue！";
             return session('wechat.oauth_user.default'); // 拿到授权用户资料
 
         });
