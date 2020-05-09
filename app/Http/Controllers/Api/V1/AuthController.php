87a39ed82b065c3df16d11fddbc65f364083d880
柚子 <106157421@qq.com>
2020-05-09 10:34:29 +0800

    完善授权/获取openid/获取手机号码

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -6,6 +6,8 @@ use App\Http\Requests\AuthMlOpenidStoreRequest;
 use App\Http\Requests\AuthPhoneStoreRequest;
 use App\Http\Requests\AuthRequest;
 use App\Http\Requests\MlOpenidAuthRequest;
+use App\Models\Team;
+use App\Models\TeamMember;
 use App\Models\User;
 use App\Transformers\UserTransformer;
 use Illuminate\Http\Request;
@@ -17,7 +19,15 @@ class AuthController extends Controller
     // 用户登陆 自己登陆$sendInviteSetId 默认是老板,超级管理员权限
     public function store(AuthRequest $request,User $user)
     {
-        $user = $user->createUser($request->phone,0,1,false,User::REFUND_STATUS_ADMINISTRATOR,$request->code);
+        $parent = $request->parent_id;
+        $sendInviteSetId = $request->send_invite_set_id;
+        $isOpen = $parent ? true : false;
+        $status = $sendInviteSetId == 1 ? User::REFUND_STATUS_MEMBER : User::REFUND_STATUS_ADMINISTRATOR;
+        $user = $user->createUser($request->phone,$request->parent_id,$request->send_invite_set_id,$isOpen,$status,$request->code);
+
+        if($teamMember = TeamMember::where('user_id',$parent)->firstOrFail()) {
+            $this->teamMember($user,$teamMember->team_id);
+        }
         $token = \Auth::guard('api')->fromUser($user);
         return $this->respondWithToken($token)->setStatusCode(201);
     }
@@ -26,7 +36,14 @@ class AuthController extends Controller
     {
         $app = app('wechat.mini_program');
         $sessionUser = $app->auth->session($request->code);
-        return ['ml_openid'=>$sessionUser['openid']];
+        $user = User::where('openid', $sessionUser['openid'])->first();
+        if($user) {
+            if (TeamMember::where('user_id', $user->id)->exists()) {
+                return $this->mlOpenid($sessionUser['openid']);
+            }
+        }
+        $token = \Auth::guard('api')->fromUser($user);
+        return $this->respondWithToken($token,null);
     }
     public function phoneStore(AuthPhoneStoreRequest $request)
     {
@@ -45,6 +62,7 @@ class AuthController extends Controller
         return $decryptedData;
     }
 
+
     // 个人中心
     public function meShow()
     {
@@ -55,12 +73,21 @@ class AuthController extends Controller
         Auth::guard('api')->logout();
         return $this->response->noContent();
     }
-    protected function respondWithToken($token)
+
+    protected function respondWithToken($token,$mlOpenid)
     {
         return $this->response->array([
+            'ml_openid' => $mlOpenid,
             'access_token' => $token,
             'token_type' => 'Bearer',
             'expires_in' => Auth::guard('api')->factory()->getTTL() * 120
+        ]);
+    }
+
+    protected function mlOpenid($mlOpenid)
+    {
+        return $this->response->array([
+            'ml_openid' => $mlOpenid,
         ]);
     }
 }
