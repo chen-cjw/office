3b5a6a1a7f6a3aaaa788c3fea5bfc7c5d53940a7
柚子 <106157421@qq.com>
2020-06-06 15:32:07 +0800

    参数写错了

--- app/Http/Controllers/Api/V1/WechatPayController.php
+++ app/Http/Controllers/Api/V1/WechatPayController.php
@@ -51,7 +51,7 @@ class WechatPayController extends Controller
             'total_fee' => 1,//$wechatPay->total_fee * 100,
             //'spbill_create_ip' => '123.12.12.123', // 可选，如不传该参数，SDK 将会自动获取相应 IP 地址
             'notify_url' => route('api.wechat_pay.handle_paid_notifies'), // 支付结果通知网址，如果不设置则会使用配置里的默认地址
-            'openid' => auth('api')->user()->openid,
+            'openid' => auth('api')->user()->ml_openid,
             'trade_type' => 'JSAPI', // 请对应换成你的支付方式对应的值类型
         ]);
         return $result;
