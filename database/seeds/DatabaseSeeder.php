8afffa8aff1973308333df320302d8b1350027a2
柚子 <106157421@qq.com>
2020-06-12 10:21:32 +0800

    会员假数据

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//         $this->call(UserSeeder::class);
-         $this->call(SendInviteSetSeeder::class);
+        $this->call(SendInviteSetSeeder::class);
+        $this->call(UserSeeder::class);
          $this->call(AdminTablesSeeder::class);
     }
 }
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -11,16 +11,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::create([
-            'openid'=>'123123123123',
-            'nickname'=>'nickname',
-            'sex'=>'sex',
-            'language'=>'zh',
-            'city'=>'chain',
-            'province'=>'jiangsu',
-            'country'=>'xuxi',
-            'avatar'=>'https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2471723103,4261647594&fm=26&gp=0.jpg',
-//            'unionid'=>'unionid'
-        ]);
+        factory(\App\Models\User::class, 1)->create();
+
     }
 }
