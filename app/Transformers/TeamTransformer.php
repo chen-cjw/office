f438786d2c58f7c55f2b315292712beb8c3b008d
柚子 <106157421@qq.com>
2020-07-13 15:11:28 +0800

    判断是免费还是团队

--- app/Transformers/TeamTransformer.php
+++ app/Transformers/TeamTransformer.php
@@ -14,11 +14,13 @@ class TeamTransformer extends TransformerAbstract
         $closeTime = strtotime(auth('api')->user()->teams[0]->close_time);
         $createTime = strtotime(auth('api')->user()->teams[0]->created_at);
         $teamCloseTime = date('Y-m-d',$closeTime);// 2020-10-10
+        $bccomp = bccomp(strtotime($teamCloseTime),strtotime(date('Y-m-d',$createTime).' + 30 day'));
         return [
             'id' => $team->id,
             'name' => $team->name,
-            'number_count' => bccomp(strtotime($teamCloseTime),strtotime(date('Y-m-d',$createTime).' + 30 day')) == 0? '无限' : $team->number_count,
+            'number_count' => $bccomp == 0? '无限' : $team->number_count,
             'close_time' => $team->close_time,
+            'is_team_version' => $bccomp == 0 ? false : true,
             'created_at' => $team->created_at->toDateTimeString(),
             'updated_at' => $team->updated_at->toDateTimeString(),
         ];
